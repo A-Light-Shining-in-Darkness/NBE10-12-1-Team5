71c8e855b8d11803de34557433926de8ad0151de
A-Light-Shining-in-Darkness <kor2002rea@naver.com>
2026-06-06 17:40:54 +0900

    refactor: API 경로 /api/v1/users -> /api/users 변경

--- back/src/main/java/com/back/domain/users/controller/UserController.java
+++ back/src/main/java/com/back/domain/users/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Tag(name = "UserController", description = "API 유저 컨트롤러")
 public class UserController {
